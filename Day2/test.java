54cca65ad257089dae52051bd5bc742a3827d227
Suyog0103 <131379044+Suyog0103@users.noreply.github.com>
2023-05-24 20:07:46 +0530

    Day2

---
+++ Day2/test.java
@@ -0,0 +1,36 @@
+
+
+public class test {
+
+	public static void main(String[] args) {
+		SCLL suyog = new SCLL();
+//		suyog.insertAtBeg(11);
+//		suyog.insertAtBeg(12);
+//		suyog.insertAtBeg(13);
+//		suyog.insertAtBeg(14);
+//		suyog.insertAtBeg(15);
+//		suyog.insertAtBeg(16);
+		
+		suyog.insertAtLast(21);
+		suyog.insertAtLast(22);
+		suyog.insertAtLast(23);
+		suyog.insertAtLast(24);
+		suyog.insertAtLast(25);
+
+//		suyog.deleteAtFirst();
+//		suyog.deleteAtLast();
+//		suyog.deleteAtPos(2);
+		
+//		suyog.insertAtPos(45, 2);
+		
+//		suyog.insertAtPos(60, 9);
+		
+//		suyog.reverse();
+		
+		suyog.search(26);
+		
+		suyog.display();
+
+	}
+
+}
